38cf28bf766d928f4845ba187f007df2b383de25
Anton Ganhammar <ganhammar@gmail.com>
2023-09-23 08:42:11 +0200

    fix: stack tests

--- src/stack/test/stack.test.ts
+++ src/stack/test/stack.test.ts
@@ -1,17 +1,26 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { WhatDidIDoWebStack } from '../lib/what-did-i-do-web-stack';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 
 jest.mock('aws-cdk-lib/aws-s3-deployment');
 
 describe('WhatDidIDoWebStack', () => {
-  it('creates the expected s3 bucket', () => {
-    const app = new App();
-    const stack = new WhatDidIDoWebStack(app, 'MyTestStack');
-    const template = Template.fromStack(stack);
+  ['Login', 'Account', 'Landing'].forEach((name) => {
+    it(`creates the ${name.toLowerCase()} s3 bucket`, () => {
+      const app = new App();
+      const stack = new WhatDidIDoWebStack(app, 'MyTestStack');
+      const template = Template.fromStack(stack);
 
-    template.hasResourceProperties('AWS::S3::Bucket', {
-      AccessControl: 'Private',
+      template.hasResourceProperties('AWS::S3::Bucket', {
+        AccessControl: 'Private',
+      });
+
+      const bucket = stack.node.tryFindChild(name);
+
+      expect(bucket).toBeDefined();
+      expect(bucket instanceof Bucket).toBe(true);
+      expect(bucket?.node.defaultChild instanceof CfnBucket).toBe(true);
     });
   });
 });
